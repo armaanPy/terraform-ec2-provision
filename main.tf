384017f3c682236403e20365576e7c9109c71bd8
armaanPy <52916251+armaanPy@users.noreply.github.com>
2022-04-09 12:20:24 +0200

    Adding elastic_ip resource.

--- main.tf
+++ main.tf
@@ -46,3 +46,8 @@ resource "aws_key_pair" "deployer" {
   key_name   = var.key_name
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsTwpJZjJ2ZQI3/Ac/SKRb96+2xC86VMN8N4ld6duYdIQdUlg3Omp914o7JHPl3PWFS54Tk3KamN6MownXTC2WWiRHvIWLYNXfISGXNGRisK7Ems6INCI8SGsbZLoIC59uRWBgqwmlrFv4+y57kpZIQlpp8m3QnaKDpkcNifhNi30xgCCc2R8xN2Hj/YddYIxIF4oMBYlzNtHBngXVQhzm8c0FJ16gGkbiejt1ejkQZetCATFPRqTkGrlPuHi6ucTwtgN8a0JGYOb0Dbzqq5ot8hIzrI1w5KVtyyRYtXq6qT4FGMiAZYUMp0tXJN7dpExtGuaDKHk99tQW6B1XgE+r ec2-user@ip-172-31-11-149.ap-east-1.compute.internal"
 }
+
+resource "aws_eip" "elastic_ip" {
+  instance = aws_instance.blockchain_host.id
+  vpc      = true
+}
