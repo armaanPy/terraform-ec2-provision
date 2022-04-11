7204865b6acaf07b1871e18cd02f35c218e34b27
armaanPy <52916251+armaanPy@users.noreply.github.com>
2022-04-11 11:39:28 +0200

    Removing pub key

--- main.tf
+++ main.tf
@@ -42,10 +42,10 @@ resource "aws_security_group" "main" {
 }
 
 
-resource "aws_key_pair" "deployer" {
-  key_name   = var.key_name
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZlbbdfzA7dWnXfgnlX2AvLpFBGGFd+ku9VAi4GNk6sooqzHTn6JwUb5dSNt0Ua7MwdcLUVDZ8SBdcEDEsivNLoka3V/wWaRGy7CfP4E5gSrh5I0j9y1nmGQiermr5X2l/kq6VnyNY23f935NaAEfA/Ticsl8YeUkkikwLhes9dlKqZAO6t5ngxauUM0SMEgcMLf6AzQ2S45SHp2laLAfMhv5IyF/Zwf8AoGKCVALG2Kl1guqhkBQpMjrKZlv8LjXhSm+IWPRXjnPWC0yrSYSDlJmQRPfDS6klKRZW6QyVbCzwVxFwnieIPiXv6tcCqOWh+W63rawLKK7lse11soBT ec2-user@ip-172-31-11-149.ap-east-1.compute.internal"
-}
+#resource "aws_key_pair" "deployer" {
+#  key_name   = var.key_name
+#  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZlbbdfzA7dWnXfgnlX2AvLpFBGGFd+ku9VAi4GNk6sooqzHTn6JwUb5dSNt0Ua7MwdcLUVDZ8SBdcEDEsivNLoka3V/wWaRGy7CfP4E5gSrh5I0j9y1nmGQiermr5X2l/kq6VnyNY23f935NaAEfA/Ticsl8YeUkkikwLhes9dlKqZAO6t5ngxauUM0SMEgcMLf6AzQ2S45SHp2laLAfMhv5IyF/Zwf8AoGKCVALG2Kl1guqhkBQpMjrKZlv8LjXhSm+IWPRXjnPWC0yrSYSDlJmQRPfDS6klKRZW6QyVbCzwVxFwnieIPiXv6tcCqOWh+W63rawLKK7lse11soBT ec2-user@ip-172-31-11-149.ap-east-1.compute.internal"
+#}
 
 resource "aws_eip" "elastic_ip" {
   instance = aws_instance.blockchain_host.id
